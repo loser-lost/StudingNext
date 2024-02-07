450a8a2d353bb7231c269ce8ff40eba2f4473b15
Michael Israel Sousa Santos <43841525+loser-lost@users.noreply.github.com>
2024-02-07 11:33:10 -0300

    link

---
+++ 1_hello-word/app/products/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+export default function Produtos() {
+    return (
+        <>
+        <h1>Produtos</h1>
+        <ul>
+            <li>
+            <Link href="/">Voltar</Link>
+            </li>
+        </ul>
+        
+        
+        </>
+        
+    )
+}
